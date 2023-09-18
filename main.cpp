b3cbeeae9bf48e81377b07118e68735d82b3249d
khgfkufigzufg <vincekiss1414@gmail.com>
2023-09-18 10:39:04 +0200

    talált hibák

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#teszt
+
 constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];                       //NELEMENTS hiányzik egy "_"
+    std::cout << '1-100 ertekek duplazasa'             //"; hiánya" string + kettős aposztróf + std::endl 
+    for (int i = 0;)                                   //nem teljes ciklus + 1től induljon
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)                            // hibás ciklus i<=N_ELEMENTS hiányzik 
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"                          //innen hiányzik az érték "b[i]"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)               //ciklusban ";" + 1től induljon amíg nem <=
     {
-        atlag += b[i]
+        atlag += b[i]                                  // ";" hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
